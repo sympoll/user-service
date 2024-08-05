c690706563af04c1fd5e10fb8bd49b3ca64ab47d
Roy <roytol12@gmail.com>
2024-08-05 16:42:25 +0300

    Delete the SQL entities, services and repositories (temporary)

--- src/main/java/com/MTAPizza/Sympoll/usermanagementservice/controller/ServiceController.java
+++ src/main/java/com/MTAPizza/Sympoll/usermanagementservice/controller/ServiceController.java
@@ -1,8 +1,5 @@
 package com.MTAPizza.Sympoll.usermanagementservice.controller;
 
-import com.MTAPizza.Sympoll.usermanagementservice.service.role.RoleService;
-import com.MTAPizza.Sympoll.usermanagementservice.service.user.UserService;
-import com.MTAPizza.Sympoll.usermanagementservice.service.userRoles.UserRolesService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +8,5 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class ServiceController {
-   private final UserService userService;
-   private final RoleService roleService;
-   private final UserRolesService userRolesService;
 
 }
