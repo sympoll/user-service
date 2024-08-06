fcde859c2f6aeb9c2191d978febafd023cba738d
Roy <roytol12@gmail.com>
2024-08-06 19:17:53 +0300

    Fix user_roles table syntax

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -13,9 +13,9 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE user_roles (
-    group_id UUID NOT NULL ,
+    group_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    role_id UUID REFERENCES roles(role_id) ON DELETE CASCADE NOT NULL,
+    role_id UUID REFERENCES roles(role_id) ON DELETE CASCADE,
     PRIMARY KEY (group_id, user_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE NOT NULL
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
