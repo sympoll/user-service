2e968c297fb9556653c5df423c97be7807845eac
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-01 14:36:24 +0300

    Fixed tests not working as a result of users table that was not updated.

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -3,6 +3,8 @@ CREATE TABLE users (
    user_id UUID PRIMARY KEY,
    username VARCHAR(255) UNIQUE NOT NULL,
    password_hash VARCHAR(255) NOT NULL,
+   profile_picture_url  VARCHAR(255),
+   banner_picture_url   VARCHAR(255),
    email VARCHAR(255),
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
