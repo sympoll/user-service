97f236e61fac6c1421ca6114b5b1a50f16ec858e
Ronenii <ronen.glm@gmail.com>
2024-09-10 02:33:52 +0300

    - Changed to post mapping

--- src/main/java/com/MTAPizza/Sympoll/usermanagementservice/config/WebConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/usermanagementservice/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost")
+                .allowedOrigins("http://localhost:8080", "http://localhost", "http://sympoll.ddns.net")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
